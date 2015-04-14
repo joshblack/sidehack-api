948bc184a0515049c41ffc21b446c7fe488b2438
Josh Black <joshuatblack@ufl.edu>
2015-04-13 21:44:45 -0400

    Adding response utility helper to clean up responses in API endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import sendResponse from '../lib/response';
 
 const router = Router();
 
-let privateFields = ['createdAt', 'updatedAt', 'token', 'projects', 'inspect', 'id'];
+let privateFields = [
+  'createdAt',
+  'updatedAt',
+  'token',
+  'projects',
+  'inspect',
+  'id'
+];
 
 function removePrivateFields(record) {
   return Object.keys(record)
@@ -21,59 +29,29 @@ router.get('/', (req, res) => {
     .then((records) => {
       let users = records.map(removePrivateFields);
 
-      res.set({
-        'Access-Control-Allow-Origin': process.env.CLIENT_URL
-      });
-      res.status(200);
-      res.json({
-        status: 200,
-        users: users
-      });
+      sendResponse(res, 200, { users });
     })
     .catch((err) => {
-      res.set({
-        'Access-Control-Allow-Origin': process.env.CLIENT_URL
-      });
-      res.status(500);
-      res.json({
-        status: 500,
+      sendResponse(res, 500, {
         message: 'Internal server error. Request could not be fulfilled.'
-      })
+      });
     });
 });
 
 router.get('/info', (req, res) => {
-  res.status(200);
-  res.json({
-    status: 200,
-    message: 'User endpoint for the Sidehack API'
-  });
+  sendResponse(res, 200, { message: 'User endpoint for the Sidehack API' });
 });
 
 router.get('/:id', (req, res) => {
   const { user: User } = req.app.models;
 
   User.findOne({ id: req.params.id })
-    .then((user) => {
-      res.set({
-        'Access-Control-Allow-Origin': process.env.CLIENT_URL
-      });
-      res.status(200);
-      res.json({
-        status: 200,
-        user: removePrivateFields(user)
-      });
-    })
+    .then((user) => sendResponse(res, 200, { user: removePrivateFields(user) }))
     .catch((err) => {
-      res.set({
-        'Access-Control-Allow-Origin': process.env.CLIENT_URL
-      });
-      res.status(400);
-      res.json({
-        status: 400,
+      sendResponse(res, 400, {
         message: 'Unsupported request for the given user. Either a user does'
           + ' not exist with the given id or another error occured.'
-      })
+      });
     });
 })
 
@@ -82,23 +60,9 @@ router.get('/:id/picture', (req, res) => {
   const { user: User } = req.app.models;
 
   User.findOne({ id: req.params.id })
-    .then((user) => {
-      res.set({
-        'Access-Control-Allow-Origin': process.env.CLIENT_URL
-      });
-      res.status(200);
-      res.json({
-        status: 200,
-        url: user.avatar_url
-      });
-    })
+    .then((user) => sendResponse(res, 200, { url : user.avatar_url }))
     .catch((err) => {
-      res.set({
-        'Access-Control-Allow-Origin': process.env.CLIENT_URL
-      });
-      res.status(400);
-      res.json({
-        status: 400,
+      sendResponse(res, 400, {
         message: "Unsupported request for the given user's picture. Either the"
         + " id is incorrect or another issue occured."
       })
